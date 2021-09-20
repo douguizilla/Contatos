8f3067ca52cce46c3fd23d93dcd5b8f1f3897907
douguizilla <douglasgommez@gmail.com>
2021-09-20 16:42:48 -0300

    set viewBinding

--- app/src/main/java/com/odougle/contatos/MainActivity.kt
+++ app/src/main/java/com/odougle/contatos/MainActivity.kt
@@ -2,10 +2,14 @@ package com.odougle.contatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.odougle.contatos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
