098777980a3b2bbadb7cc09667ebeebb049d3592
douguizilla <douglasgommez@gmail.com>
2021-09-20 17:48:14 -0300

    implemeted ContactsActivity.kt

--- app/src/main/java/com/odougle/contatos/ContactsActivity.kt
+++ app/src/main/java/com/odougle/contatos/ContactsActivity.kt
@@ -1,15 +1,65 @@
 package com.odougle.contatos
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.odougle.contatos.databinding.ActivityContactsBinding
 
 class ContactsActivity : AppCompatActivity() {
-    private val binding: ActivityContactsBinding by lazy{
+    private val binding: ActivityContactsBinding by lazy {
         ActivityContactsBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
     }
+
+    override fun onResume() {
+        super.onResume()
+        if (!hasPermission(Manifest.permission.WRITE_CONTACTS)) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS),
+            RC_PERMISSION_CONTACT)
+        }else{
+            init()
+        }
+    }
+
+    private fun hasPermission(permission: String): Boolean {
+        return ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == RC_PERMISSION_CONTACT && grantResults.isNotEmpty()){
+            if(!grantResults.all { it == PackageManager.PERMISSION_GRANTED }){
+                Toast.makeText(this, R.string.error_permission, Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun init(){
+        if(supportFragmentManager.findFragmentByTag(TAG_CONTACT_LIST) == null){
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, ContactListFragment(), TAG_CONTACT_LIST)
+                .commit()
+        }
+    }
+
+    companion object{
+        private const val TAG_CONTACT_LIST = "contacts_fragment"
+        private const val RC_PERMISSION_CONTACT = 1
+    }
+
 }
