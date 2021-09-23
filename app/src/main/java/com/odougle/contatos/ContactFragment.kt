2f991004a9bcf4e1d8b8a2a147c74937f61fffc9
douguizilla <douglasgommez@gmail.com>
2021-09-23 16:15:26 -0300

    implemented onCreateDialog

--- app/src/main/java/com/odougle/contatos/ContactFragment.kt
+++ app/src/main/java/com/odougle/contatos/ContactFragment.kt
@@ -1,9 +1,35 @@
 package com.odougle.contatos
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.content.DialogInterface
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.fragment.app.DialogFragment
+import com.odougle.contatos.databinding.FragmentContactBinding
 
 class ContactFragment : DialogFragment(), DialogInterface.OnClickListener {
+
+    private lateinit var binding: FragmentContactBinding
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        binding = FragmentContactBinding.inflate(requireActivity().layoutInflater)
+        binding.imgPhoto.setOnClickListener {
+            selectPhotoFromGallery()
+        }
+        return AlertDialog.Builder(requireContext())
+            .setTitle(R.string.dialog_title)
+            .setView(binding.root)
+            .setPositiveButton(R.string.button_ok, this)
+            .setNegativeButton(R.string.button_cancel, null)
+            .create()
+    }
+
+    private fun selectPhotoFromGallery() {
+        TODO("Not yet implemented")
+    }
 
     override fun onClick(dialog: DialogInterface?, which: Int) {
         TODO("Not yet implemented")
