c3b0233600031b4e7de3c09b56f425eb271782cd
douguizilla <douglasgommez@gmail.com>
2021-09-23 15:57:57 -0300

    created ContactFragment.kt

---
+++ app/src/main/java/com/odougle/contatos/ContactFragment.kt
@@ -0,0 +1,11 @@
+package com.odougle.contatos
+
+import android.content.DialogInterface
+import androidx.fragment.app.DialogFragment
+
+class ContactFragment : DialogFragment(), DialogInterface.OnClickListener {
+
+    override fun onClick(dialog: DialogInterface?, which: Int) {
+        TODO("Not yet implemented")
+    }
+}
