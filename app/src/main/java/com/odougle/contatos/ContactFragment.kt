a2048e3ee2acace991ce102536eb3328d4eb8e9b
douguizilla <douglasgommez@gmail.com>
2021-09-23 16:24:46 -0300

    implemented onClick

--- app/src/main/java/com/odougle/contatos/ContactFragment.kt
+++ app/src/main/java/com/odougle/contatos/ContactFragment.kt
@@ -10,10 +10,12 @@ import android.widget.EditText
 import android.widget.ImageView
 import androidx.fragment.app.DialogFragment
 import com.odougle.contatos.databinding.FragmentContactBinding
+import com.odougle.contatos.util.ContactUtils
 
 class ContactFragment : DialogFragment(), DialogInterface.OnClickListener {
 
     private lateinit var binding: FragmentContactBinding
+    private var selectedPhoto: Bitmap? = null
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         binding = FragmentContactBinding.inflate(requireActivity().layoutInflater)
@@ -35,6 +37,17 @@ class ContactFragment : DialogFragment(), DialogInterface.OnClickListener {
     }
 
     override fun onClick(dialog: DialogInterface?, which: Int) {
-        TODO("Not yet implemented")
+        val photo = selectedPhoto
+        binding.apply {
+            if(edtName.text.isNotEmpty() && edtAddress.text.isNotEmpty() && photo != null){
+                ContactUtils.insetContact(
+                    requireContext(),
+                    edtName.text.toString(),
+                    edtPhone.text.toString(),
+                    edtAddress.text.toString(),
+                    photo
+                )
+            }
+        }
     }
 }
